db94b2caac90263377246182babd7c0472e3b0d4
Sutou Kouhei <kou@clear-code.com>
2021-11-11 16:10:42 +0900

    Fix name

--- lib/groonga-import/version.rb
+++ lib/groonga-import/version.rb
@@ -13,6 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-module GroongaSynonym
-  VERSION = "1.0.1"
+module GroongaImport
+  VERSION = "1.0.0"
 end
